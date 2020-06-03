2aa4146651214475efc472c857e34b3c14bf72fe
dimker <dimker4@gmail.com>
2020-06-03 09:44:50 +0500

    lesson7_2

--- src/ru/geekbrains/lesson7/Cat.java
+++ src/ru/geekbrains/lesson7/Cat.java
@@ -1,4 +1,37 @@
 package ru.geekbrains.lesson7;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety = false; // При создании кошки, она голодная
+
+    public boolean getSatiety () {
+        return this.satiety;
+    }
+
+    public int getAppetite () {
+        return this.appetite;
+    }
+
+    public Cat (String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.getFood() >= this.appetite) { // Если еды в миске больше чем аппетит, то будем кушать
+            plate.decreaseFood(this.appetite);
+            this.satiety = true; // Сделем кота сытым
+            System.out.println(this.name + " скушал " + this.appetite);
+
+        } else {
+            this.satiety = false; // Оставим кота голодным
+            System.out.println(this.name + " хотел скушать " + this.appetite + ", но в тарелке осталось " + plate.getFood());
+        }
+    }
+
+    public void printSatiety() {  // Печать сытости котика
+        System.out.println( (this.satiety) ? this.name + " сытый": this.name + " остался голодный :(");
+    }
+
 }
--- src/ru/geekbrains/lesson7/Plate.java
+++ src/ru/geekbrains/lesson7/Plate.java
@@ -1,4 +1,35 @@
 package ru.geekbrains.lesson7;
 
 public class Plate {
+    private int food;
+
+    public int getFood() // Добавим геттер для обращения к приватному полю
+    {
+        return food;
+    }
+
+    public Plate (int food) {
+        this.food = food;
+    }
+
+    public void info () {
+        System.out.println("В тарелке осталось " + this.food);
+    }
+
+    public void decreaseFood (int animalAppetite) {
+        if (this.food >= animalAppetite && animalAppetite > 0) { // Запретим отрицательное количество еды в миске
+            this.food -= animalAppetite;
+        } else {
+            System.out.println("Невозможно скушать больше, чем есть еды в миске");
+        }
+    }
+
+    public void addFood (int food) {
+        if (food > 0) {
+            this.food += food;
+            System.out.println("В тарелку добавили " + food + " еды");
+        } else {
+            System.out.println("Нельзя положить в тарелку значение меньше 1");
+        }
+    }
 }
